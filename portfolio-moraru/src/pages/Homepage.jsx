3f32ad1a13c206be35394c01a116057dd6f0f028
Steopa2001 <moraru495@gmail.com>
2025-11-10 17:20:24 +0100

    [feat] update CV file

--- portfolio-moraru/src/pages/Homepage.jsx
+++ portfolio-moraru/src/pages/Homepage.jsx
@@ -37,7 +37,7 @@ export default function Homepage() {
                   Guarda i progetti
                 </a>
                 <a
-                  href={`${BASE}CV-Moraru-Stefan.pdf`}
+                  href={`${BASE}Moraru-Stefan-cv-aggiornato.pdf`}
                   className="btn btn-outline-primary"
                   target="_blank"
                   rel="noreferrer"
