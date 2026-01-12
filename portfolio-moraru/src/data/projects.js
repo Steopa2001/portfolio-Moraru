2adc787555ab7b50e78b8290bade6e6d1b11a0ff
Steopa2001 <moraru495@gmail.com>
2026-01-12 19:03:59 +0100

    [fix] remove outdated project entry for Arcadia

--- portfolio-moraru/src/data/projects.js
+++ portfolio-moraru/src/data/projects.js
@@ -11,15 +11,6 @@ const projectsData = [
     codeUrl: "https://github.com/Steopa2001/html-css-spotifyweb",
   },
   {
-    title: "Project Work - Arcadia",
-    description:
-      "E-commerce di giochi da tavolo sviluppato in team durante il corso Boolean. Mi sono occupato della parte front-end, realizzando i componenti principali in React e gestendo le funzionalità interattive con Axios e Bootstrap, in integrazione con un back-end Node.js/Express/MySQL.",
-    tech: ["React", "Axios", "Javascript", "Bootstrap", "CSS"],
-    image: `${BASE}Arcadia-project-work.png`,
-    demoUrl: "https://arcadia-react-production.up.railway.app/",
-    codeUrl: "https://github.com/Steopa2001/Arcadia-react",
-  },
-  {
     title: "SmartphoneHub – Comparatore",
     description:
       "SPA sviluppata con React e Vite che permette di sfogliare, filtrare e confrontare smartphone, salvare i preferiti e visualizzare i dettagli di ogni modello. Front-end in React con backend JSON fittizio esposto via API.",
